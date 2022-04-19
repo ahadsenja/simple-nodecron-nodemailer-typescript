afa657a7c8a1fb789e7595c6fb31309dd3305313
ahadsenja <hamdanilm96@gmail.com>
2022-04-20 00:00:29 +0800

    initial project

---
+++ app.ts
@@ -0,0 +1,40 @@
+const cron = require("node-cron");
+const nodemailer = require('nodemailer');
+
+class EmailMailer {
+  task = cron.schedule("09 23 * * *", () => {
+    this.sendMail().then(() => {
+      console.log('Success send email')
+    });
+  });
+
+  sendMail = async () => {
+    let transaction, income, configMail, transporter, emailTarget, mail;
+
+    transaction = Math.floor(Math.random() * 10) + 1;
+    income = `Rp ${transaction * 10000},00`;
+
+    configMail = {
+      service: 'gmail',
+      auth: {
+        user: 'learns.daniel@gmail.com',
+        pass: '124n13lfcb.112'
+      }
+    };
+
+    transporter = await nodemailer.createTransport(configMail);
+    emailTarget = 'hamdanilm96@gmail.com';
+
+    mail = {
+      to: emailTarget,
+      from: configMail.auth.user,
+      subject: '[Daily Report] - Transaction & Total Income',
+      html: `This is your daily report. Total <b>Success Transaction: ${transaction}</b>`
+    };
+
+    transporter.sendMail(mail);
+  }
+}
+
+const emm = new EmailMailer().task;
+emm.start();
